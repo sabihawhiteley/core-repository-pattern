fd76423d7cb3f589d7ba7bdd2d5650d38c3ed186
sabihawhiteley <95507719+sabihawhiteley@users.noreply.github.com>
2021-12-06 17:22:57 +0300

    Create IUnitOfWork.cs

---
+++ IUnitOfWork.cs
@@ -0,0 +1,8 @@
+namespace RepositoryPattern.SampleExample
+{
+    public interface IUnitOfWork<TDbContext> where TDbContext : DbContext
+    {
+        IRepository<TEntity> GetRepositoy<TEntity>() where TEntity : EntityBase;
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
